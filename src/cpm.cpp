ac123b49f6e9023e8168f35f11fa6252367d6d91
TheScorpoi <sobral@ua.pt>
2023-03-03 19:17:32 +0000

    not 100% functional, but its in a good way

--- src/cpm.cpp
+++ src/cpm.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include "hashTable.cpp"
 
 #include <fstream>
 #include <unordered_map>
 #include <string>
+#include <list>
+#include <iterator>
+
 
 
 
@@ -31,33 +33,65 @@ void read_file(string file_name) {
     }
 }
 
+bool contains(list<string> list, string element) {
+    return find(list.begin(), list.end(), element) != list.end();
+
+}
+
 
 int main(int argc, char** argv) {
 
     unordered_map<string, list<int> > un_map;
 
-    string file_name = "o texto o texto e o texto";
-    //read_file(file_name);
+    string string_example = "o texto o texto e o texto";
+    list<string> k_word_read_vector;
+    // read_file(file_name);
 
     int k = 5;
 
-    for (int i = 0; i < file_name.length() - k; i++) {
-        string k_gram = file_name.substr(i, k);
+    // populate the map with substrings of size k, save the index of the k_word in the list
+    for (int i = 0; i < string_example.length() - k; i++) {
+        string k_string = string_example.substr(i, k);
 
-        list<int> pos;
-        if (un_map.find(k_gram) == un_map.end()){
-            pos.push_back(i);
-            un_map.insert(make_pair(k_gram, pos));
-        } else {
-            list<int> pos = un_map[k_gram];
-            pos.push_back(i);
-            un_map.insert(make_pair(k_gram, pos));
+
+        if (contains(k_word_read_vector, k_string)) {
+            un_map[k_string].push_back(i);
+            cout << "a palavra já apareceu, siga ver qual será a proxima letra" << " --  " << i << "  --  " << endl;
+
+            // ent agora temos de ir ao nosso hash e ir procurar pela palavra que ja apareceu para sabermos o indice que foi pra depois
+            // ir buscar o indice da proxima palavra pra saber qual é a mais provavel de aparecer, mas é pra ir buscar à lista (neste caso k_word_read_vector)
+
+            string word = un_map.find(k_string)->first;
+            list<int> index = un_map.find(k_string)->second; // estou só a ir buscar o ultimo index
+            cout << "a palavra é: " << word << " e o indice é " << index.back() << endl;
+
+
+            //ir a lista k_word_read_vector e ver qual é a proxima palavra mais provavel de aparecer (index + 1)
+            
+            auto it = k_word_read_vector.begin();
+            int next_index = index.back() + 1;
+            cout << "aquiiiiii " << next_index << endl;
+            advance(it, next_index);
+            cout << "a seguir a --> " << word << " <-- a palavra mais provavel de aparecer é --> " << *it << " <-- " << endl;
         }
 
-
-        un_map.insert(make_pair(k_gram, i));
+        un_map[k_string].push_back(i);
+        k_word_read_vector.push_back(k_string);
     }
 
+    cout << "\n\n\n" << endl;
+
+    int index = 0;
+    for (auto it = k_word_read_vector.begin(); it != k_word_read_vector.end(); ++it) {
+        cout << index << " -> " << *it << '\n';
+        index++;
+    }
+    
+
+    
+    cout << "\n\n\n" << endl;
+
+    // print the map
     for (auto it = un_map.begin(); it != un_map.end(); it++) {
         cout << it->first << " => ";
         for (int i : it->second) {
