0d300e9ac691be14692f6df7e02776536653f48f
TheScorpoi <sobral@ua.pt>
2023-03-06 15:57:36 +0000

    guess the next word done i think

--- src/cpm.cpp
+++ src/cpm.cpp
@@ -55,31 +55,37 @@ int main(int argc, char** argv) {
 
 
         if (contains(k_word_read_vector, k_string)) {
-            un_map[k_string].push_back(i);
             cout << "a palavra já apareceu, siga ver qual será a proxima letra" << " --  " << i << "  --  " << endl;
 
             // ent agora temos de ir ao nosso hash e ir procurar pela palavra que ja apareceu para sabermos o indice que foi pra depois
             // ir buscar o indice da proxima palavra pra saber qual é a mais provavel de aparecer, mas é pra ir buscar à lista (neste caso k_word_read_vector)
 
-            string word = un_map.find(k_string)->first;
-            list<int> index = un_map.find(k_string)->second; // estou só a ir buscar o ultimo index
-            cout << "a palavra é: " << word << " e o indice é " << index.back() << endl;
+            string word = un_map.find(k_string)->first; // dá a key do mapa
+            list<int> index = un_map.find(k_string)->second; // ir buscar os values do map (lista de indices)
+            cout << "a palavra é: " << word << " e o indice é " << index.back() << endl; // index.back() dá o ultimo index da lista de indices
 
 
             //ir a lista k_word_read_vector e ver qual é a proxima palavra mais provavel de aparecer (index + 1)
-            
-            auto it = k_word_read_vector.begin();
             int next_index = index.back() + 1;
-            cout << "aquiiiiii " << next_index << endl;
-            advance(it, next_index);
-            cout << "a seguir a --> " << word << " <-- a palavra mais provavel de aparecer é --> " << *it << " <-- " << endl;
+            
+            //auto it = k_word_read_vector.begin();
+            //cout << "aquiiiiii " << next_index << endl;
+            //advance(it, next_index);
+
+            int index2 = 0;
+            for (auto it = k_word_read_vector.begin(); it != k_word_read_vector.end(); ++it) {
+                if (index2 == next_index) {
+                    cout << "a proxima palavra mais provavel é: " << *it << endl;
+                }
+                index2++;
+            }
         }
 
         un_map[k_string].push_back(i);
         k_word_read_vector.push_back(k_string);
     }
 
-    cout << "\n\n\n" << endl;
+    cout << "\n\n" << endl;
 
     int index = 0;
     for (auto it = k_word_read_vector.begin(); it != k_word_read_vector.end(); ++it) {
@@ -89,7 +95,7 @@ int main(int argc, char** argv) {
     
 
     
-    cout << "\n\n\n" << endl;
+    cout << "\n\n" << endl;
 
     // print the map
     for (auto it = un_map.begin(); it != un_map.end(); it++) {
