63fc77171e7650b805c2d2d53e8b6acaa9b658f5
reis <joaoreis16@ua.pt>
2023-03-07 01:26:58 +0000

    calcula um prob, mas não sei se está bem

--- src/cpm.cpp
+++ src/cpm.cpp
@@ -4,10 +4,11 @@
 #include <string>
 #include <list>
 #include <iterator>
+#include <algorithm>
 
 using namespace std;
 
-///////////////////////////////////////////////////////§
+///////////////////////////////////////////////////////
 
 bool contains(list<string> list, string element);
 
@@ -19,46 +20,69 @@ void print_words_read_list(list<string> words_read);
 
 void print_unordered_map();
 
+/////////////// global variables //////////////////////
+
+static unordered_map<string, list<int> > un_map;
+static list<string> k_word_read_vector;
+static int alpha = 1;
+
 ///////////////////////////////////////////////////////
 
-// Global Variables
-static unordered_map<string, list<int> > un_map;
+float calculate_probability(string word, string next_word) {
+    int count, next_count = 0;;
 
-static list<string> k_word_read_vector;
+    for (auto it = un_map.begin(); it != un_map.end(); it++) {
+        if (it->first == word)      count++;
+        if (it->first == next_word) next_count++;
+    }
 
-///////////////////////////////////////////////////////§
+    return (float) (next_count + alpha)/(count + 2 * alpha);
+}
 
 int main(int argc, char** argv) {
 
-
-    string string_example = "o texto o texto e o texto";
-
+    if (argc < 2) {
+        printf("[usage]: %s file_path", argv[0]);
+    }
+    
+    int i = 0;
     int k = 5;
+    string word;
+    // string string_example = "o texto o texto e o texto";
 
-    for (int i = 0; i < string_example.length() - k; i++) {
-        string k_string = string_example.substr(i, k);
+    do {
+        word = read_char(argv[1], k);
+        cout << "content => " << word << endl;
 
-        if (contains(k_word_read_vector, k_string)) {
-            string word = un_map.find(k_string)->first; // dá a key do mapa
-            list<int> index = un_map.find(k_string)->second; // ir buscar os values do map (lista de indices)
+        if (word == "") break;
+
+        if (contains(k_word_read_vector, word)) {
+            string k_string = un_map.find(word)->first;         // dá a key do mapa
+            list<int> index = un_map.find(word)->second;        // ir buscar os values do map (lista de indices)
 
             int next_index = index.back() + 1;
             
+            float prob_hit;
             int index2 = 0;
             for (auto it = k_word_read_vector.begin(); it != k_word_read_vector.end(); ++it) {
                 if (index2 == next_index) {
-                    cout << "the word is: " << k_string << " the next is: " <<*it << endl;
+                    cout << "the word is: '" << word << "' the next is: '" << *it << "'" << endl;
+                    prob_hit = calculate_probability(word, *it);
                 }
                 index2++;
             }
+            cout << "P(hit) = " << prob_hit << "\n" << endl;
         }
-        un_map[k_string].push_back(i);
-        k_word_read_vector.push_back(k_string);
-    }
+
+        un_map[word].push_back(i);
+        k_word_read_vector.push_back(word);
+        i++;
+
+    } while (true);
 
     //print this just for debugging
-    //print_words_read_list(k_word_read_vector);
-    //print_unordered_map();
+    print_words_read_list(k_word_read_vector);
+    print_unordered_map();
 
     return 0;
 }
@@ -83,22 +107,21 @@ void print_unordered_map() {
     }
 }
 
- bool contains(list<string> list, string element) {
-     return find(list.begin(), list.end(), element) != list.end();
+bool contains(list<string> list, string element) {
+    return find(list.begin(), list.end(), element) != list.end();
 }
 
-void read_file(string file_name) {
+string read_file(char *file_name) {
     ifstream file;
     file.open(file_name);
-    if (file.is_open()) {
-        string line;
-        while (getline(file, line)) {
-            cout << line << endl;
-        }
-        file.close();
-    } else {
-        cout << "Unable to open file";
-    }
+
+    string str;
+    string content;
+    while (getline(file, str)) {
+        content += str;
+    } 
+
+    return content;
 }
 
 // Function to read and return "k" chars from the file every time it is called
@@ -113,7 +136,7 @@ string read_char(string file_name, int k) {
     // read k chars from the file
     char buffer[k];
     file.read(buffer, k);
-    c= string(buffer, k);
+    c = string(buffer, k);
 
     // check if the file has less than k chars
     if (file.gcount() < k) {
@@ -123,6 +146,7 @@ string read_char(string file_name, int k) {
     //if it is the end of the file, close the file
     if (file.eof()) {
         file.close();
+        return "";
     }
 
     return c;
