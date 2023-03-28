8c0fa8df23f2d709398e41fe6ac313454f0fcc31
TheScorpoi <sobral@ua.pt>
2023-03-29 00:49:13 +0100

    add bash to do graphics data

--- src/run.sh
+++ src/run.sh
@@ -5,5 +5,13 @@ make
 if [ $? == 2 ]; then
     echo "Exiting..."
 else
-    ./CPM
+    for K in {3..8}; do
+        for A in [0.25, 0,50, 0,75, 1]; do
+            for T in [1..6]; do
+                echo Executing CPM for K=$K alpha=$A threshold=$T
+                ./CPM -f ../example/chry.txt -k $K -a $A -t $T > ../results/chry_K${K}_A${A}_T${T}.txt
+            done
+        done
+    done
 fi
+
